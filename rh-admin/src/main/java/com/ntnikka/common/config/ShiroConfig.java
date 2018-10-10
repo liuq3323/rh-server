bfa8d571af8b3225f0b08bf2b1781093cc6ef662
liuq <530775870@qq.com>
2018-10-10 09:26:34 +0800

    10.10 开放测试页面

--- rh-admin/src/main/java/com/ntnikka/common/config/ShiroConfig.java
+++ rh-admin/src/main/java/com/ntnikka/common/config/ShiroConfig.java
@@ -71,6 +71,7 @@ public class ShiroConfig {
         filterMap.put("/sys/login", "anon");
         filterMap.put("/favicon.ico", "anon");
         filterMap.put("/captcha.jpg", "anon");
+        filterMap.put("/modules/aliPayTest/**","anon");
         filterMap.put("/**", "authc");
         shiroFilter.setFilterChainDefinitionMap(filterMap);
 
